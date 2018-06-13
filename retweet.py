6058493b88c36260ffb84131f83e958fa438b24c
Niharika Shrivastava <chunnushrivastava@gmail.com>
2018-06-13 23:22:27 +0530

    update accounts list and fix bug

--- retweet.py
+++ retweet.py
@@ -8,32 +8,30 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 # Add usernames of all accounts that need to be retweeted/favourted/followed here
-account_list = ['github','Topcoder','sitepointdotcom','ConsenSys','gsoc','outreachy','WomenWhoCode']
+account_list = ['sitepointdotcom','ConsenSys','gsoc','WSJ','observer','mashable','TechCrunch','WIRED','outreachy','WomenWhoCode','Topcoder','github','TheNextWeb','tech2eets','verge','DigitalTrends','arstechnica','npmjs','mozilla']
 
-for target in account_list: 
-    print("Getting data for " + target)
-    item = api.get_user(target)
-    print("name: " + item.name)
-    print("screen_name: " + item.screen_name)
-    if not item.following:
-    	item.follow()
-    	print('Followed the user')
+while True:
+    for target in account_list:
+        print("Getting data for " + target)
+        item = api.get_user(target)
+        print("name: " + item.name)
+        print("screen_name: " + item.screen_name)
+        if not item.following:
+            item.follow()
+            print('Followed the user')
 
-    tweets = api.user_timeline( id=target, count = 1)
-    for tweet in tweets:
-    	try:
-    		tweet.retweet()
-    		print('Retweeted the tweet')
+        tweets = api.user_timeline( id=target, count = 1)
+        for tweet in tweets:
+            try:
+                tweet.retweet()
+                print('Retweeted the tweet')
 
-    		tweet.favorite()
-    		print('Favorited the tweet')
-    		sleep(7200) #2 hours between each tweet
+                tweet.favorite()
+                print('Favorited the tweet')
+                sleep(7200) #after every 2 hours
 
-    	except tweepy.TweepError as e:
-    	    print(e.reason)
+            except tweepy.TweepError as e:
+                print(e.reason)
 
-    	except StopIteration:
-    	    break
-    sleep(86400) #start tweeting round after a day
-
-
+            except StopIteration:
+                break
